c85020c367856bae26496e7ccb636fec6dba6b67
Annay <paulannay14@gmail.com>
2021-06-29 16:48:06 +0600

    Lesson 5: Function Expressions

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("Say hi to node.js!");
+/*
+    Lesson 5: Function Expressions
+*/
+
+// Normal Functions
+function greeting() {
+  console.log("Hi, there!");
+}
+greeting();
+
+// Function Expression
+var message = function () {
+  console.log("This is a message");
+};
+message();
+
+// Normal Function call within a  function
+function printGreeting() {
+  greeting();
+}
+printGreeting();
+
+// Node specific call within a function
+var printMessage = function (text) {
+  text();
+};
+printMessage(message);
