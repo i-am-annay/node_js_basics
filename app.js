af1f2305a48ca12e538c23c949da340908256ef8
Annay <paulannay14@gmail.com>
2021-06-29 16:26:21 +0600

    Lesson 4: Global Object

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log("Say hi to node.js!");
+/*
+    Lesson 4: The Global Object
+
+    Global Object is similar to Window object in java script.
+    
+*/
+
+/*
+
+    Demonstrating How Some properties of global object works in Node.js
+
+*/
+
+// console          - For Logging messages to the console
+console.log("This is a message");
+
+// setTimeOut - For running a function after a preferred time
+setTimeout(function () {
+  console.log("3 Seconds Have passed");
+}, 3000);
+
+// setInterval      - For running a function repeatedly after a certain time interval
+// clearInterval    - For removing/clearing the interval after execution
+var time = 0;
+var timer = setInterval(function () {
+  time += 2;
+  console.log(time + "seconds have passed");
+  if (time > 5) {
+    clearInterval(timer);
+  }
+}, 2000);
+
+// __dirname        - For getting the current working directory
+// __filename       - For getting the current working file
+
+console.log(__dirname);
+console.log(__filename);
+
+// Documentation: https://nodejs.org/api/globals.html
