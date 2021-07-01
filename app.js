c13409929c7efac159bdcfbc183e04a51695133b
Annay <paulannay14@gmail.com>
2021-07-01 13:54:43 +0600

    Lesson 11 to 14: Clients and Servers, Creating a Server, Streams and Buffers, Readable Streams

--- app.js
+++ app.js
@@ -14,3 +14,14 @@ var server = http.createServer(function (request, response) {
 //listen('portNumber', 'ip of localhost')   - Serves as a listener
 server.listen(3000, "127.0.0.1");
 console.log("Listening to port 30000");
+
+/*
+    Lesson 14: Readable Streams
+*/
+var http = require("http");
+var fs = require("fs");
+
+var myReadStream = fs.createReadStream(__dirname + "/readMe.txt", "utf-8");
+myReadStream.on("data", function (data) {
+  console.log(data);
+});
