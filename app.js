a2cec03a77b27331fc566a2d9611dd1681b2bc0f
Annay <paulannay14@gmail.com>
2021-07-01 13:35:05 +0600

    Another Commit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Say hi to node.js!");
+/*
+    Lesson 12: Creating A Server
+*/
+
+var http = require("http");
+
+//createSerever         - Creates a Server, takes a function as argument which takes in request and the response
+var server = http.createServer(function (request, response) {
+  console.log("Request was made: " + request.url); // Logs after every refresh / request
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("Hi people!");
+});
+
+//listen('portNumber', 'ip of localhost')   - Serves as a listener
+server.listen(3000, "127.0.0.1");
+console.log("Listening to port 30000");
